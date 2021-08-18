da7ba6e92b464c1f014ae662bbc8731ecb80127a
Hendra Anggrian <hendraanggrian@gmail.com>
2021-08-19 03:56:23 +0700

    Generated by gradle-git-publish.

---
+++ bannerbar-core/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":bannerbar-ktx:dokkaHtml/androidTestRelease":[],":bannerbar-ktx:dokkaHtml/debug":[],":bannerbar-ktx:dokkaHtml/main":[],":bannerbar-ktx:dokkaHtml/release":[]}'
